abc19c4c024dd73cb07e28c61f86a1f123b10fc2
A_D <aunderscored@gmail.com>
2021-12-07 09:25:38 +0200

    cleanup

--- aoc/python/seven.py
+++ aoc/python/seven.py
@@ -18,38 +18,28 @@ class CrabSub:
 
 
 def part_1(input: str) -> str:
-    input = TEST_INPUT
     positions = [CrabSub(int(p)) for p in input.split(",")]
 
-    print(f"{positions[0]}", positions[0].fuel_to_move(5))
-
-    best = -1
     best_fuel = -1
 
     for target in range(min(c.position for c in positions), max(c.position for c in positions)):
         fuel_required = sum(c.fuel_to_move(target) for c in positions)
 
         if fuel_required < best_fuel or best_fuel == -1:
-            best = target
             best_fuel = fuel_required
 
-    return f"{best=} {best_fuel=}"
+    return f"{best_fuel}"
 
 
 def part_2(input: str) -> str:
-    # input = TEST_INPUT
     positions = [CrabSub(int(p)) for p in input.split(",")]
 
-    best = -1
     best_fuel = -1
 
     for target in range(min(c.position for c in positions), max(c.position for c in positions)):
         fuel_required = sum(c.fuel_to_move(target, exp=True) for c in positions)
 
         if fuel_required < best_fuel or best_fuel == -1:
-            best = target
             best_fuel = fuel_required
 
-    return f"{best=} {best_fuel=}"
-
-    return ""
+    return f"{best_fuel}"
